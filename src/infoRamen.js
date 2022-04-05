84be7914234ade48955d3dc39a7b3d1c5c4d358b
gali1917 <carlgale93@hotmail.com>
2022-04-05 18:06:04 -0500

    Arreglo de IMGs, info sushi

---
+++ src/infoRamen.js
@@ -0,0 +1,19 @@
+function ramenPollo(){
+    const img = document.getElementById("imgPollo");
+    (img.style.display == "block")
+    ? img.style.display = "none"
+    : img.style.display = "block";
+}
+
+function ramenMar(){
+    const img = document.getElementById("imgMar");
+    (img.style.display == "block")
+    ? img.style.display = "none"
+    : img.style.display = "block";
+}
+function ramenMixto(){
+    const img = document.getElementById("imgMixto");
+    (img.style.display == "block")
+    ? img.style.display = "none"
+    : img.style.display = "block";
+}
---
+++ src/infoSushi.js
@@ -0,0 +1,19 @@
+function breakTempura(){
+    const img = document.getElementById("imgBreakTempura");
+    (img.style.display == "block")
+    ? img.style.display = "none"
+    : img.style.display = "block";
+}
+
+function breakMix(){
+    const img = document.getElementById("imgBreakMix");
+    (img.style.display == "block")
+    ? img.style.display = "none"
+    : img.style.display = "block";
+}
+function breakFresh(){
+    const img = document.getElementById("imgBreakFresh");
+    (img.style.display == "block")
+    ? img.style.display = "none"
+    : img.style.display = "block";
+}
