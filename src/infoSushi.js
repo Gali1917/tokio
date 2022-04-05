b980250c2e913b1438bf75982b2724e75b5d6449
gali1917 <carlgale93@hotmail.com>
2022-04-05 18:37:47 -0500

    Adicion de sushi

--- src/infoSushi.js
+++ src/infoSushi.js
@@ -1,19 +1,43 @@
-function breakTempura(){
-    const img = document.getElementById("imgBreakTempura");
+function sushi(id){
+    const img = document.getElementById(id);
     (img.style.display == "block")
     ? img.style.display = "none"
     : img.style.display = "block";
 }
 
+function breakTempura(){
+    const id = "imgBreakTempura";
+    sushi(id);
+}
 function breakMix(){
-    const img = document.getElementById("imgBreakMix");
-    (img.style.display == "block")
-    ? img.style.display = "none"
-    : img.style.display = "block";
+    const id = "imgBreakMix";
+    sushi(id);
 }
 function breakFresh(){
-    const img = document.getElementById("imgBreakFresh");
-    (img.style.display == "block")
-    ? img.style.display = "none"
-    : img.style.display = "block";
+    const id = "imgBreakFresh";
+    sushi(id);
+}
+function otakuTempura(){
+    const id = "imgOtakuTempura";
+    sushi(id);
+}
+function otakuMix(){
+    const id = "imgOtakuMix";
+    sushi(id);
+}
+function otakuFresh(){
+    const id = "imgOtakuFresh";
+    sushi(id);
+}
+function parcheTempura(){
+    const id = "imgParcheTempura";
+    sushi(id);
+}
+function parcheMix(){
+    const id = "imgParcheMix";
+    sushi(id);
+}
+function parcheFresh(){
+    const id = "imgParcheFresh";
+    sushi(id);
 }
