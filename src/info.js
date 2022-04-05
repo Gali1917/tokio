f584b41bc7e9d2b2ef0a3d644be93641be2c75f1
gali1917 <carlgale93@hotmail.com>
2022-04-04 21:34:56 -0500

    Agrego tipos de ramen

---
+++ src/info.js
@@ -0,0 +1,18 @@
+function ramenPollo(){
+    const img = document.getElementById("imgPollo");
+    (img.style.display == "none") 
+    ? img.style.display = "block" 
+    : img.style.display = "none";
+}
+function ramenMar(){
+    const img = document.getElementById("imgMar");
+    (img.style.display == "none") 
+    ? img.style.display = "block" 
+    : img.style.display = "none";
+}
+function ramenMixto(){
+    const img = document.getElementById("imgMixto");
+    (img.style.display == "none") 
+    ? img.style.display = "block" 
+    : img.style.display = "none";
+}
