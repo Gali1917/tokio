7c2c93b2f1bc80d80a12503483e007700a8fffcb
gali1917 <carlgale93@hotmail.com>
2022-04-08 17:05:20 -0500

    Trabajo en el Desk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function all(textSet, imgSrc, button, textA){
+function all(textSet, imgSrc, button, textA, iFrame){
     const back = document.getElementById("back");
     const p = document.getElementById("pMain");
     const text = textSet;
@@ -9,6 +9,8 @@ function all(textSet, imgSrc, button, textA){
     p.textContent = text;
     const aLink = document.getElementById("infoA");
     aLink.href = textA;
+    const i = document.getElementById("iFrame");
+    i.src = iFrame;
     back.style.display = "block";
     back.style.top = "0";
     back.style.position = "fixed";
@@ -18,36 +20,40 @@ function ramen(){
     const imgSrc = "https://i.postimg.cc/YqRzjfr3/pexels-cats-coming-400.jpg";
     const button = "Tipos de Ramen";
     const a = "./src/infoRamen.html";
-    all(textSet, imgSrc, button, a);
+    const iFrame = "./src/infoRamenDesk.html";
+    all(textSet, imgSrc, button, a, iFrame);
 }
 function sushi(){
     const textSet = "Es un plato típico de origen japonés basado en arroz aderezado con vinagre de arroz, azúcar y sal y combinado con otros ingredientes como pescados crudos, mariscos, verduras, etc. Este plato es uno de los más reconocidos de la gastronomía japonesa y uno de los más populares internacionalmente.";
     const imgSrc = "https://i.postimg.cc/Tw7VYypt/pexels-alexandr-podvalny400.jpg";
     const button = "Tipos de Sushi";
     const a = "./src/infoSushi.html";
-    all(textSet, imgSrc, button, a);
+    const iFrame = "./src/infoSushiDesk.html";
+    all(textSet, imgSrc, button, a, iFrame);
 }
 function arroz(){
     const textSet = "El arroz japonés es el elemento esencial de la comida japonesa, hasta tal punto que su denominación gohan, sirve tanto para arroz blanco como para comida. En Japón se cultiva y come principalmente dos tipos de arroz.";
     const imgSrc = "https://i.postimg.cc/Hx6x07MX/pexels-richard-vijay-400.jpg";
     const button = "Tipos de Arroz";
     const a = "./src/infoArroz.html";
-    all(textSet, imgSrc, button, a);
+    const iFrame = "./src/infoArrozDesk.html";
+    all(textSet, imgSrc, button, a, iFrame);
 }
 function entradas(){
     const textSet = "Que te parece una deliciosa entrada mientras esperas tu comida, o un magnifico postre para cerrar la velada? No te pierdas nuestros deliciosos snacks.";
     const imgSrc = "https://i.postimg.cc/mgVCph7Z/pexels-zeynep-aytekin-400.jpg";
     const button = "Tipos de Entradas";
     const a = "./src/infoEntradas.html";
-
-    all(textSet, imgSrc, button, a);
+    const iFrame = "./src/infoEntradasDesk.html";
+    all(textSet, imgSrc, button, a, iFrame);
 }
 function bebidas(){
     const textSet = "Las bebidas típicas de Japón son parte de su cultura y del protocolo tradicional de la tierra del sol naciente. Son muchas, desde licores como el sake, derivado del arroz, hasta innumerables cervezas locales.";
     const imgSrc = "https://i.postimg.cc/XqckVLBd/pexels-christina-petsos-400.jpg";
     const button = "Tipos de Bebidas";
     const a = "./src/infoBebidas.html";
-    all(textSet, imgSrc, button, a);
+    const iFrame = "./src/infoBebidasDesk.html";
+    all(textSet, imgSrc, button, a, iFrame);
 }
 function buttonClose(){
     const back = document.getElementById("back");
---
+++ src/infoBebidas.js
@@ -0,0 +1,43 @@
+function sushi(id){
+    const img = document.getElementById(id);
+    (img.style.display == "block")
+    ? img.style.display = "none"
+    : img.style.display = "block";
+}
+
+function breakTempura(){
+    const id = "imgBreakTempura";
+    sushi(id);
+}
+function breakMix(){
+    const id = "imgBreakMix";
+    sushi(id);
+}
+function breakFresh(){
+    const id = "imgBreakFresh";
+    sushi(id);
+}
+function otakuTempura(){
+    const id = "imgOtakuTempura";
+    sushi(id);
+}
+function otakuMix(){
+    const id = "imgOtakuMix";
+    sushi(id);
+}
+function otakuFresh(){
+    const id = "imgOtakuFresh";
+    sushi(id);
+}
+function parcheTempura(){
+    const id = "imgParcheTempura";
+    sushi(id);
+}
+function parcheMix(){
+    const id = "imgParcheMix";
+    sushi(id);
+}
+function parcheFresh(){
+    const id = "imgParcheFresh";
+    sushi(id);
+}
